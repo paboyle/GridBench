ca922a2a5ac644f0978f428af927674b8e1ddc96
u37294 <u37294@s001-n140.aidevcloud>
2020-03-24 10:57:53 -0700

    Updated

---
+++ arch/gen64/static_data.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+const uint64_t nsite = 512 ; 
+const int Ls = 8 ; 
+extern double   U_static[] ; 
+extern double   Phi_static[] ; 
+extern double   Psi_cpp_static[] ; 
+extern uint64_t nbr_static[] ; 
+extern uint8_t  prm_static[] ; 
