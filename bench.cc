0c5ec9e731a7717d81c1d3f54550d84e6d93dccd
Peter Boyle <paboyle@ph.ed.ac.uk>
2020-07-20 08:17:36 -0700

    Repliica count to grow problem size

--- bench.cc
+++ bench.cc
@@ -55,28 +55,45 @@ int main(int argc, char* argv[])
   ////////////////////////////////////////////////////////////////////
   // Option 2: copy from static arrays
   ////////////////////////////////////////////////////////////////////
+  uint64_t nreplica = 16;
   uint64_t umax   = nsite*18*8 *vComplexD::Nsimd(); 
   uint64_t fmax   = nsite*24*Ls*vComplexD::Nsimd(); 
   uint64_t nbrmax = nsite*Ls*8;
   uint64_t vol    = nsite*Ls*vComplexD::Nsimd(); 
-
+  
   printf("Nsimd %d\n",vComplexD::Nsimd());
-  
-  Vector<double>   U(umax);       bcopy(U_static,&U[0],umax*sizeof(double));
-  Vector<double>   Psi(fmax);     bzero(&Psi[0],fmax*sizeof(double));
-  Vector<double>   Phi(fmax);     bcopy(Phi_static,&Phi[0],fmax*sizeof(double));
-  Vector<double>   Psi_cpp(fmax); bcopy(Psi_cpp_static,&Psi_cpp[0],fmax*sizeof(double));
-  Vector<uint64_t> nbr(nsite*Ls*8); bcopy(nbr_static,&nbr[0],nbrmax*sizeof(uint64_t));
-  Vector<uint8_t>  prm(nsite*Ls*8); bcopy(prm_static,&prm[0],nbrmax*sizeof(uint8_t));
 
-  Vector<float>   fU(umax);       
-  Vector<float>   fPsi(fmax);     
-  Vector<float>   fPhi(fmax);
-  Vector<float>   fPsi_cpp(fmax); 
 
+  Vector<double>   U(umax*nreplica);       
+  Vector<double>   Psi(fmax*nreplica);     
+  Vector<double>   Phi(fmax*nreplica);     
+  Vector<double>   Psi_cpp(fmax*nreplica); 
+  Vector<uint64_t> nbr(nsite*Ls*8*nreplica); 
+  Vector<uint8_t>  prm(nsite*Ls*8*nreplica); 
+
+  for(int replica=0;replica<nreplica;replica++){
+    int u=replica*umax;
+    int f=replica*fmax;
+    int n=replica*nbrmax;
+    bcopy(U_static,&U[u],umax*sizeof(double));
+    bzero(&Psi[f],fmax*sizeof(double));
+    bcopy(Phi_static,&Phi[f],fmax*sizeof(double));
+    bcopy(Psi_cpp_static,&Psi_cpp[f],fmax*sizeof(double));
+    bcopy(nbr_static,&nbr[n],nbrmax*sizeof(uint64_t));
+    bcopy(prm_static,&prm[n],nbrmax*sizeof(uint8_t));
+    for(int nn=0;nn<nbrmax;nn++){
+      nbr[nn+n]+=nsite*Ls; // Shift the neighbour indexes to point to this replica
+    }
+  }
   
+  Vector<float>   fU(umax*nreplica);       
+  Vector<float>   fPsi(fmax*nreplica);     
+  Vector<float>   fPhi(fmax*nreplica);
+  Vector<float>   fPsi_cpp(fmax*nreplica); 
+
 #ifdef RRII
   const int Nsimd = vComplexD::Nsimd();
+  for(uint32_t r=0;r<nreplica;r++){
   for(uint32_t ss=0;ss<nsite;ss++){
   for(uint32_t s=0;s<Ls;s++){
   for(uint32_t sc=0;sc<12;sc++){
@@ -85,23 +102,24 @@ int main(int argc, char* argv[])
 	int idx = ss*Ls*24*Nsimd
 	        +     s*24*Nsimd
     	        +      sc*2*Nsimd;
-	Phi    [idx + ri*Nsimd + n ] =     Phi_static[idx + n*2 + ri];
-	Psi_cpp[idx + ri*Nsimd + n ] = Psi_cpp_static[idx + n*2 + ri];
-	fPhi    [idx + ri*Nsimd + n ] =     Phi_static[idx + n*2 + ri];
-	fPsi_cpp[idx + ri*Nsimd + n ] = Psi_cpp_static[idx + n*2 + ri];
-
+	int ridx= idx+r*nsite*Ls*24*Nsimd;
+	Phi     [ridx + ri*Nsimd + n ] =     Phi_static[idx + n*2 + ri];
+	Psi_cpp [ridx + ri*Nsimd + n ] = Psi_cpp_static[idx + n*2 + ri];
+	fPhi    [ridx + ri*Nsimd + n ] =     Phi_static[idx + n*2 + ri];
+	fPsi_cpp[ridx + ri*Nsimd + n ] = Psi_cpp_static[idx + n*2 + ri];
       }
     }
-  }}}
+  }}}}
   std::cout << "Remapped Spinor data\n" ;
+  for(uint32_t r=0;r<nreplica;r++){
   for(uint32_t ss=0;ss<nsite*9*8;ss++){
     for(uint32_t n=0;n<vComplexD::Nsimd();n++){
       for(uint32_t ri=0;ri<2;ri++){
-	U[ss*Nsimd*2 + ri*Nsimd + n ] = U_static[ss*Nsimd*2 + n*2 + ri];
-	fU[ss*Nsimd*2 + ri*Nsimd + n ] = U_static[ss*Nsimd*2 + n*2 + ri];
+	U [r*Nsimd*2*nsite*9*8+ss*Nsimd*2 + ri*Nsimd + n ] = U_static[ss*Nsimd*2 + n*2 + ri];
+	fU[r*Nsimd*2*nsite*9*8+ss*Nsimd*2 + ri*Nsimd + n ] = U_static[ss*Nsimd*2 + n*2 + ri];
       }
     }
-  }
+  }}
   std::cout << "Remapped Gauge data\n";
 #endif
   
@@ -109,8 +127,8 @@ int main(int argc, char* argv[])
   std::cout << "Calling dslash_kernel "<<std::endl;
 
 
-  double flops = 1320.0*vol;
-  int nrep=10000; // cache warm
+  double flops = 1320.0*vol*nreplica;
+  int nrep=100; // cache warm
 #define DOUBLE
 #ifdef DOUBLE
   double usec = dslash_kernel<vComplexD>(nrep,
@@ -118,7 +136,7 @@ int main(int argc, char* argv[])
 			   (vComplexD *)&Psi[0],
 			   (vComplexD *)&Phi[0],
 			   &nbr[0],
-			   nsite,
+			   nsite*nreplica,
 			   Ls,
 			   &prm[0]);
 #else
@@ -127,12 +145,12 @@ int main(int argc, char* argv[])
 			   (vComplexF *)&fPsi[0],
 			   (vComplexF *)&fPhi[0],
 			   &nbr[0],
-			   nsite,
+			   nsite*nreplica,
 			   Ls,
 			   &prm[0]);
 
   // Copy back to double
-  for(uint64_t i=0; i<fmax;i++){
+  for(uint64_t i=0; i<fmax*nreplica;i++){
     Psi[i]=fPsi[i];
   }
 #endif
@@ -146,23 +164,25 @@ int main(int argc, char* argv[])
   std::cout << std::endl;
 
   // Check results
-  double err=0;
-  double nref=0;
-  double nres=0;
 
   vComplexD *Psi_p = (vComplexD *) &Psi[0];
   vComplexD *Psi_cpp_p = (vComplexD *) &Psi_cpp[0];
-  for(uint64_t i=0; i<fmax;i++){
-    err += (Psi_cpp[i]-Psi[i])*(Psi_cpp[i]-Psi[i]);
-    nres += Psi[i]*Psi[i];
-    nref += Psi_cpp[i]*Psi_cpp[i];
-  };
-
-  std::cout<< "normdiff "<< err<< " ref "<<nref<<" result "<<nres<<std::endl;
-  //  for(int i=0;i<fmax;i++){
-  for(int i=0;i<20;i++){
-    std::cout<< i<<" ref "<<Psi_cpp[i]<< " result "<< Psi[i]<<std::endl;
+  for(uint64_t r=0; r<nreplica;r++){
+    double err=0;
+    double nref=0;
+    double nres=0;
+    for(uint64_t ii=0; ii<fmax;ii++){
+      uint64_t i=ii+r*fmax;
+      err += (Psi_cpp[i]-Psi[i])*(Psi_cpp[i]-Psi[i]);
+      nres += Psi[i]*Psi[i];
+      nref += Psi_cpp[i]*Psi_cpp[i];
+    };
+    std::cout<< "normdiff "<< err<< " ref "<<nref<<" result "<<nres<<std::endl;
+    for(int ii=0;ii<20;ii++){
+      uint64_t i=ii+r*fmax;
+      std::cout<< i<<" ref "<<Psi_cpp[i]<< " result "<< Psi[i]<<std::endl;
+    }
   }
-  assert(err <= 1.0e-6);  
+  //  assert(err <= 1.0e-6);  
   return 0;
 }
